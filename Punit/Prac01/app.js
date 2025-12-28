94420e3e8f3e4348ecb0bb074670f8326a930dc7
Punit_singh <mr.punitsingh2628@gmail.com>
2025-12-28 14:16:43 +0530

    Add files via upload

---
+++ Punit/Prac01/app.js
@@ -0,0 +1,8 @@
+  const canva=document.getElementsById("myclass");
+ const ctx=canva.getContext("2d");
+      ctx.beginPath();
+      ctx.moveTo(410,2);
+      ctx.lineTo(410,410);
+      ctx.strokeStyle="green";
+      ctx.lineWidth= 10;
+      ctx.stroke();
